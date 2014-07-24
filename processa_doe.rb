44e3025cc9e2fc34c3d2742425cb10b781882902
Ricidleiv Tondatto <tondatto@gmail.com>
2014-07-24 17:58:21 -0400

    Correção nome constante URL

--- processa_doe.rb
+++ processa_doe.rb
@@ -4,15 +4,19 @@ require "open-uri"
 XPATH_INSCRICAO = "//tr[position() > 1]/td[2]/p/span/text()"
 
 print "Informe o número da Matéria: "
-@materia_id = gets
-print "Informe o número da Edicação: "
-@edicao_id = gets
+@materia_id = gets.chomp
+print "Informe o número da Edição: "
+@edicao_id = gets.chomp
 
 BASE_IOMAT_URL = "http://www.iomat.mt.gov.br/do/navegadorhtml"
 BASE_IOMAT_ATO_URL = "#{BASE_IOMAT_URL}/mostrar.htm?id=#{@materia_id}&edi_id=#{@edicao_id}"
 
-page = Nokogiri::HTML(open(BASE_IOMAT_ATO_PAGE))
+page = Nokogiri::HTML(open(BASE_IOMAT_ATO_URL))
+elementos = page.xpath(XPATH_INSCRICAO)
 
-for node in page.xpath(XPATH_INSCRICAO)
+puts "Encontrado #{elementos.length} nomeados neste ato."
+puts "Lista de inscrições:"
+
+for node in elementos
 	puts node.to_s
 end
